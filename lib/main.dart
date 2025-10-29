0cd4d878f52836aa064f6412c491fc52eb257134
sotatt <pg6eat5f@i.softbank.jp>
2025-10-29 14:13:40 +0900

    一旦全部上げますね

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './view/page/navibar.dart';
+import './view/page/sign/start.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: Navibar(),
+      home: StartPage(),
     );
   }
 }
--- lib/view/page/sign/signIn.dart
+++ lib/view/page/sign/signIn.dart
@@ -1,1 +1,27 @@
 //ログイン画面
+import 'package:flutter/material.dart';
+
+class SignIn extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Text(
+                'Sign In',
+                style: TextStyle(
+                  fontFamily: "Corporate Logo Rounded Bold",
+                  fontSize: 40,
+                ),
+              ),
+              /* ここに、ヘッダー・フォーム・フッターを並べていく */
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
