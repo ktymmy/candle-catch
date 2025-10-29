ef8ab016cadd9c5ddb107e08120fdfa396b32ce6
ktymmy <ktymmy0121@gmail.com>
2025-10-29 14:10:50 +0900

    navibar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './view/page/navibar.dart';
+// import './const/colorConst.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,6 +16,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        // scaffoldBackgroundColor: ColorConst.main,
       ),
       home: Navibar(),
     );
